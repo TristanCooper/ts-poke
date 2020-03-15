b810c8acce1c2d6aadbc479c26f0cf064d6b2d81
Tristan Cooper <tjcoops1796@gmail.com>
2020-03-15 22:10:04 +0000

    more

--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -2,11 +2,29 @@ import React, { Component } from 'react'
 import User from '../interfaces/User.Interface';
 
 export class PokemonSearch extends Component<User> {
+	pokemonRef: React.RefObject<HTMLInputElement>;
+
+	constructor(props: User) {
+		super(props);
+		this.state = {
+			name: '',
+			numberOfAbilities: null,
+			baseExp: null,
+			imageUrl: ''
+		}
+
+		this.pokemonRef = React.createRef();
+	}
+
 	render() {
 		const { name, numberOfPokemon } = this.props;
 		return (
 			<div>
 				<p>{name} {numberOfPokemon && <span>{numberOfPokemon} Pokemon</span>}</p>	
+				<input type="text" ref={this.pokemonRef} />
+				<button onClick={this.onSearchClick} className="search-button">
+					Search
+				</button>
 			</div>
 		)
 	}
